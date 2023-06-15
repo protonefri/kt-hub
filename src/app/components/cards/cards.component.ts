bd9d49946e16ecdc1386c77954831c1a42e2febe
Faris Škrijelj <faris.skrijelj@fincore.com>
2023-06-15 14:49:41 +0200

    overhaul

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,7 +1,15 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Card, Ploy } from './card';
 import { FormControl, FormGroup } from '@angular/forms';
 import html2canvas from 'html2canvas';
+import { allData } from 'src/assets/compendium';
 
 @Component({
   selector: 'app-cards',
@@ -10,12 +18,23 @@ import html2canvas from 'html2canvas';
 })
 export class CardsComponent implements OnInit {
   dual: boolean = false;
+  allData: any = allData;
+  cardType!: string;
+  factions!: any;
+  operatives!: any;
+  @Output() ployEmitter = new EventEmitter<string>();
 
   @ViewChild('firstCard', { static: false }) firstCard!: ElementRef;
 
   backgroundColor: any = '#c54c21';
   fontColor: any = '#ffffff';
 
+  weaponForm = new FormGroup({
+    wFaction: new FormControl(),
+    wKillTeam: new FormControl(),
+    wOperative: new FormControl(),
+    wPloy: new FormControl(),
+  });
 
   card: Card = {
     name: '',
@@ -28,7 +47,7 @@ export class CardsComponent implements OnInit {
     weapons: undefined,
     abilities: undefined,
     uniqueActions: undefined,
-    width: 630 ,
+    width: 630,
     height: 880,
   };
 
@@ -59,7 +78,31 @@ export class CardsComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.weaponForm.get('wOperative')?.valueChanges.subscribe((value) => {
+      console.log(value);
+      this.card.APL = value.APL;
+      this.card.DF = value.DF;
+      this.card.GA = value.GA;
+      this.card.movement = value.M.slice(0, 1);
+      this.card.name = value.opname;
+      this.card.save = value.SV;
+      this.card.wounds = value.W;
+      this.card.weapons = value?.weapons;
+      this.card.abilities = value?.abilities;
+      this.card.uniqueActions = value?.uniqueactions;
+    });
+    this.weaponForm.get('wFaction')?.valueChanges.subscribe((value) => {
+      this.factions = value.killteams;
+    });
+    this.weaponForm.get('wKillTeam')?.valueChanges.subscribe((value) => {
+      this.operatives = value.fireteams[0]?.operatives;
+      this.ploys = value.ploys.strat.concat(value.ploys.tac);
+      //this.ployEmitter.emit(this.ploys);
+    });
+
+
+  }
 
   downloadImage() {
     html2canvas(this.firstCard.nativeElement).then((canvas) => {
@@ -71,7 +114,7 @@ export class CardsComponent implements OnInit {
     });
   }
 
-  print(style:any){
-    console.log(style);
+  typeSelect(value: any) {
+    this.cardType = value;
   }
 }
--- src/app/components/cards/selection-forms/operative-selection/operative-selection.component.ts
+++ src/app/components/cards/selection-forms/operative-selection/operative-selection.component.ts
@@ -7,57 +7,31 @@ import { allData } from 'src/assets/compendium';
 @Component({
   selector: 'app-operative-selection',
   templateUrl: './operative-selection.component.html',
-  styleUrls: ['./operative-selection.component.scss']
+  styleUrls: ['./operative-selection.component.scss'],
 })
 export class OperativeSelectionComponent implements OnInit {
   @Input() card!: Card;
   @Input() type!: string;
-  @Input() ploys:any;
-  @Output() ployEmitter = new EventEmitter<string>();
+  @Input() ploys: any;
+  @Input() weaponForm!: FormGroup;
+  @Input() allData!: any;
+  imp: any;
 
+  constructor() {}
 
-  constructor() { }
-  allData: any = allData;
-  imp: any = this.allData[2]
-
-  factions!:any;
-  operatives!:any;
+  @Input() factions!: any;
+  @Input() operatives!: any;
   //killteams[7].fireteams[0].operatives;
 
   ngOnInit(): void {
     console.log(this.allData);
-    this.weaponForm.get('wOperative')?.valueChanges.subscribe(value => {
-      console.log(value)
-      this.card.APL = value.APL;
-      this.card.DF = value.DF;
-      this.card.GA = value.GA;
-      this.card.movement = value.M.slice(0,1);
-      this.card.name = value.opname;
-      this.card.save = value.SV;
-      this.card.wounds = value.W;
-      this.card.weapons = value?.weapons;
-      this.card.abilities = value?.abilities;
-      this.card.uniqueActions = value?.uniqueactions;
-    })
+    debugger;
 
-    this.weaponForm.get('wFaction')?.valueChanges.subscribe(value => {
-      this.factions = value.killteams;
-    })
-    this.weaponForm.get('wKillTeam')?.valueChanges.subscribe(value => {
-      this.operatives = value.fireteams[0]?.operatives;
-      this.ploys = value.ploys.strat.concat(value.ploys.tac);
-      this.ployEmitter.emit(this.ploys);
-    })
-    this.weaponForm.get('wPloy')?.valueChanges.subscribe(value => {
+
+    this.weaponForm.get('wPloy')?.valueChanges.subscribe((value) => {
       this.card.ploy = value;
-    })
+    });
 
+    this.imp = this.allData[2];
   }
-
-  weaponForm = new FormGroup({
-    wFaction: new FormControl(),
-    wKillTeam: new FormControl(),
-    wOperative: new FormControl(),
-    wPloy: new FormControl()
-  });
 }
